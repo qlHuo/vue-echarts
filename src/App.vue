a1e36cb05d8d4337b713afc157cb51f65953b565
霍青利 <1322346367@qq.com>
2020-10-20 10:34:41 +0800

    组件整体样式布局

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SalesReport> </SalesReport>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SalesReport from './components/SalesReport'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SalesReport
   }
 }
 </script>
 
 <style>
+html, body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  background-color: #ccc;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/SalesReport/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="sale-reports">
+    <div class="header"></div>
+    <div class="content"></div>
+    <div class="footer"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+.sale-reports {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  box-shadow: 0 2px 8px rgba(4, 5, 9, .7);
+  .header {
+    width: 100%;
+    height: 50px;
+    border-bottom: 1px solid #ccc;
+    box-sizing: border-box;
+    // background-color: red;
+  }
+  .content {
+    flex: 1;
+    width: 100%;
+    // background-color: green;
+  }
+  .footer {
+    width: 100%;
+    height: 120px;
+    border-top: 1px solid #ccc;
+    box-sizing: border-box;
+    // background-color: blue;
+  }
+
+}
+  
+</style>
